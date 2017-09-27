ca0c6a93c2118429ea693f016fab5c597cc6e4aa
jfredrickson <jfredric@gmail.com>
2017-09-27 11:32:48 -0700

    p1: view contoller sub classes added for table views and their cells. Basic alert implemented for add input (missing text field).

---
+++ To Do List/ListsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListsTableViewCell.swift
+//  To Do List
+//
+//  Created by Joshua Fredrickson on 9/27/17.
+//  Copyright © 2017 Joshua Fredrickson. All rights reserved.
+//
+
+import UIKit
+
+class ListsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ To Do List/TasksTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TasksTableViewCell.swift
+//  To Do List
+//
+//  Created by Joshua Fredrickson on 9/27/17.
+//  Copyright © 2017 Joshua Fredrickson. All rights reserved.
+//
+
+import UIKit
+
+class TasksTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
